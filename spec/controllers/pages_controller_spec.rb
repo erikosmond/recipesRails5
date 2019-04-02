896934932f3cc93af4174a9bcc55fb695bc9c975
Erik Osmond <erikosmond@gmail.com>
2019-04-01 22:38:03 -0400

    unsync tagtypes for pages controller spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -6,6 +6,7 @@ require 'rails_helper'
 describe PagesController, type: :controller do
   let!(:user) { create(:user) }
   let!(:recipe) { create(:recipe) }
+  let!(:recipe2) { create(:recipe, name: 'recipe 2') }
   let!(:tag) { create(:tag) }
 
   let!(:ratings_type) { create(:tag_type, name: 'Rating') }
@@ -13,35 +14,56 @@ describe PagesController, type: :controller do
   let!(:priority_type) { create(:tag_type, name: 'Priority') }
   let!(:priority_tag) { create(:tag, tag_type: priority_type, name: 'a priority') }
   let!(:ing_family_type) { create(:tag_type, name: 'IngredientFamily') }
+  let!(:ing_type_type) { create(:tag_type, name: 'IngredientType') }
+  let!(:ing_type) { create(:tag_type, name: 'Ingredient') }
   let!(:family_tag) { create(:tag, tag_type: ing_family_type, name: 'fam tag') }
-  let!(:tag_selection) { create(:tag_selection, tag: family_tag, taggable: recipe) }
-  let!(:access) { create(:access, accessible: tag_selection, user: user) }
-  let(:all_tag_types) do
+  let!(:type_tag) { create(:tag, tag_type: ing_type_type, name: 'type tag') }
+  let!(:ing_tag) { create(:tag, tag_type: ing_type, name: 'ing type') }
+  let!(:type_to_family) { create(:tag_selection, tag: family_tag, taggable: type_tag) }
+  let!(:ing_to_type) { create(:tag_selection, tag: type_tag, taggable: ing_tag) }
+  let!(:tag_selection) { create(:tag_selection, tag: ing_tag, taggable: recipe2) }
+  let!(:family_tag_selection) { create(:tag_selection, tag: family_tag, taggable: recipe) }
+  let!(:access1) { create(:access, accessible: family_tag_selection, user: user) }
+  let!(:access2) { create(:access, accessible: tag_selection, user: user) }
+  let!(:access3) { create(:access, accessible: type_to_family, user: user) }
+  let!(:access4) { create(:access, accessible: ing_to_type, user: user) }
+  let!(:all_tag_types) { TagType.all }
+  let!(:all_tags) { Tag.all }
+  let!(:tag_types_by_id) do
     {
-      TagType.first.id => TagType.first.name,
-      TagType.second.id => TagType.second.name,
-      TagType.third.id => TagType.third.name,
-      TagType.fourth.id => TagType.fourth.name
+      all_tag_types[0].id => all_tag_types[0].name,
+      all_tag_types[1].id => all_tag_types[1].name,
+      all_tag_types[2].id => all_tag_types[2].name,
+      all_tag_types[3].id => all_tag_types[3].name,
+      all_tag_types[4].id => all_tag_types[4].name,
+      all_tag_types[5].id => all_tag_types[5].name
     }
   end
-  let(:tags_by_type) do
+  let!(:tags_by_type) do
     {
       TagType.first.id => [Tag.first.id],
       TagType.second.id => [Tag.second.id],
       TagType.third.id => [Tag.third.id]
     }
   end
-  let(:all_tags) do
+  let!(:tags_by_id) do
     {
-      Tag.first.id => Tag.first.name,
-      Tag.second.id => Tag.second.name,
-      Tag.third.id => Tag.third.name,
-      Tag.fourth.id => Tag.fourth.name
+      all_tags[0].id => all_tags[0].name,
+      all_tags[1].id => all_tags[1].name,
+      all_tags[2].id => all_tags[2].name,
+      all_tags[3].id => all_tags[3].name,
+      all_tags[4].id => all_tags[4].name,
+      all_tags[5].id => all_tags[5].name
     }
   end
-  let(:tag_groups) { 'fix test setup' }
+  let!(:tag_groups) do
+    { family_tag.id => { type_tag.id => [ing_tag.id] } }
+  end
 
   describe 'GET - home' do
+    before(:each) do
+      TagType.unsync_ids
+    end
     before do
       sign_in user
       get :home,
@@ -55,14 +77,17 @@ describe PagesController, type: :controller do
           format: 'html'
     end
 
-    it { expect(response.status).to eq(200) }
-    it { expect(assigns[:all_tags]).to eq(all_tags) }
+    it { expect(response.status).to eq(200) } # safe
+    it { expect(assigns[:all_tags]).to eq(tags_by_id) } # safe
     it { expect(assigns[:home_tag_id]).to eq(tag.id) }
-    it { expect(assigns[:all_tag_types]).to eq(all_tag_types) }
+    it { expect(assigns[:all_tag_types]).to eq(tag_types_by_id) }
+
     it { expect(assigns[:tag_groups]).to eq(tag_groups) }
-    it { expect(assigns[:tags_by_type]).to eq(tags_by_type) }
-    it { expect(assigns[:ratings]).to eq(rating_tag.name => rating_tag.id) }
-    it { expect(assigns[:priorities]).to eq(priority_tag.name => priority_tag.id) }
+
+    it { expect(assigns[:tags_by_type]).to eq(tags_by_type) } # safe
+    it { expect(assigns[:ratings]).to eq(rating_tag.name => rating_tag.id) } # safe
+
+    it { expect(assigns[:priorities]).to eq(priority_tag.name => priority_tag.id) } # safe?
 
     it { is_expected.to render_template :home }
   end
